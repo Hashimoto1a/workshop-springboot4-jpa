cbd3dd9a74d78a94ded3823b9f1e6b06ec4d49b4
Hashimoto1a <newtondainercia@gmail.com>
2026-04-12 09:47:41 -0300

    H2 database, test profile, JPA

--- src/main/java/oi/github/hashimoto1a/course/entities/User.java
+++ src/main/java/oi/github/hashimoto1a/course/entities/User.java
@@ -3,10 +3,20 @@ package oi.github.hashimoto1a.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "tb_user")
 public class User implements Serializable{ //Serializable para transformar os objetos de uma classe
 										   //em uma cadeia de bytes e poder trafegar na rede entre outras ações
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
