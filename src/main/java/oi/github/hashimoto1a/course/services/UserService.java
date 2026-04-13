8a67bfbf2ac5d5256411115171e18daa0842815e
Hashimoto1a <newtondainercia@gmail.com>
2026-04-13 20:07:23 -0300

    Service layer, component registration

---
+++ src/main/java/oi/github/hashimoto1a/course/services/UserService.java
@@ -0,0 +1,30 @@
+package oi.github.hashimoto1a.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import oi.github.hashimoto1a.course.entities.User;
+import oi.github.hashimoto1a.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	@GetMapping
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
