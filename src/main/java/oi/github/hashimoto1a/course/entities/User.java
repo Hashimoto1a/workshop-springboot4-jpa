bb1fa5844ed6d589fef8b6beebfc53fa68bbaeee
Hashimoto1a <newtondainercia@gmail.com>
2026-04-15 16:43:09 -0300

    Order, Instant, ISO 8601 - Part1

--- src/main/java/oi/github/hashimoto1a/course/entities/User.java
+++ src/main/java/oi/github/hashimoto1a/course/entities/User.java
@@ -1,12 +1,15 @@
 package oi.github.hashimoto1a.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class User implements Serializable{ //Serializable para transformar os ob
 	private String email;
 	private String fone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -73,7 +79,11 @@ public class User implements Serializable{ //Serializable para transformar os ob
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	public List<Order> getOrders(){
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
